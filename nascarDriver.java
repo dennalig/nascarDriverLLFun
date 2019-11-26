4f92e2e28ca3fb74c98613a698d820b6ee48a279
Dennali Grissom <dennaligrissom7@gmail.com>
2019-11-26 00:41:12 -0500

    .

---
+++ nascarDriver.java
@@ -0,0 +1,38 @@
+public class nascarDriver{
+  
+    nascarDriver next;
+    int number;
+    String driverName;
+    int chasePoints;
+
+
+    public nascarDriver(int number, String driverName,int chasePoints){
+        this.number=number;
+        this.driverName=driverName;
+        this.chasePoints= chasePoints;
+
+    }
+
+    public static void main(String[] args){
+        startingLineup Daytona= new startingLineup();
+        
+        
+
+        Daytona.addToField(11, "Denny Hamlin",645965);
+        Daytona.addToField(88, "Alex Bowman", 545353);
+        Daytona.addToField(9, "Chase Elliot", 5466);
+        Daytona.addToField(4, "Kevin Harvick",5646);
+        Daytona.addToField(14, "Martin Truex Jr.",88969);
+        Daytona.addToField(22, "Joey Logano",58358);
+
+
+    Daytona.sortByNumberAscending();
+    // Daytona.removeDriverWithNumber(11);
+    // Daytona.sortByNumberDescending();
+    // Daytona.removeRacerFromField("Denny Hamlin");
+    // Daytona.removeEvenPlacedRacers();
+    Daytona.printField();
+    }
+
+}
+
---
+++ startingLineup.java
@@ -0,0 +1,136 @@
+public class startingLineup{
+
+    public nascarDriver polePosition;  //head
+
+    public void addToField(int number, String name, int chasePoints){
+        nascarDriver currentDriver=polePosition;
+
+        if (polePosition==null){
+            polePosition=new nascarDriver(number, name, chasePoints);
+            return;
+        }
+        while(currentDriver.next!=null){
+            currentDriver=currentDriver.next;
+        }
+        currentDriver.next= new nascarDriver(number, name,chasePoints);
+        return;
+    }
+
+    public void printField(){
+        nascarDriver currentDriver=polePosition;
+
+        System.out.println("---Starting Field---");
+    int fieldPos=0;
+        while(currentDriver!=null){
+            fieldPos++;
+            System.out.println(fieldPos+ ": "+ currentDriver.driverName+ ", "+ currentDriver.number + " Pts.: "+ currentDriver.chasePoints);
+            currentDriver=currentDriver.next;
+        }
+    }
+
+     public void sortByNumberAscending(){
+                nascarDriver currentDriver=null;
+                int tempDriverNumber=0;
+                String tempDriverName="";
+                int tempCP=0;
+                boolean checkSwitch=false;
+            
+
+                do{
+
+                    checkSwitch=false;
+                    for(currentDriver=polePosition;currentDriver!=null;currentDriver=currentDriver.next){ //for loop that loops through linked list
+                        if(currentDriver.next!=null&& currentDriver.next.number<currentDriver.number){ //checks that node.next!=null
+
+                        tempDriverNumber=currentDriver.number;
+                        tempDriverName=currentDriver.driverName;
+                        tempCP=currentDriver.chasePoints;
+
+                        currentDriver.number=currentDriver.next.number;
+                        currentDriver.driverName=currentDriver.next.driverName;
+                        currentDriver.chasePoints=currentDriver.next.chasePoints;
+
+                        currentDriver.next.number=tempDriverNumber;
+                        currentDriver.next.driverName=tempDriverName;
+                        currentDriver.next.chasePoints=tempCP;
+
+                        checkSwitch=true;
+                        }
+                        
+                    }
+                }while(checkSwitch);
+            }
+
+    public void sortByNumberDescending(){
+
+        nascarDriver currentDriver=null;
+        int tempDriverNumber=0;
+        String tempDriverName="";
+        int tempCP=0;
+        boolean checkSwitch=false;
+    
+
+        do{
+
+            checkSwitch=false;
+            for(currentDriver=polePosition;currentDriver!=null;currentDriver=currentDriver.next){ //for loop that loops through linked list
+                if(currentDriver.next!=null&& currentDriver.next.number>currentDriver.number){ //checks that node.next!=null
+
+                tempDriverNumber=currentDriver.number;
+                tempDriverName=currentDriver.driverName;
+                tempCP=currentDriver.chasePoints;
+
+                currentDriver.number=currentDriver.next.number;
+                currentDriver.driverName=currentDriver.next.driverName;
+                currentDriver.chasePoints=currentDriver.next.chasePoints;
+
+                currentDriver.next.number=tempDriverNumber;
+                currentDriver.next.driverName=tempDriverName;
+                currentDriver.next.chasePoints=tempCP;
+
+                checkSwitch=true;
+                }
+                
+            }
+        }while(checkSwitch);
+        
+    }
+
+    public void removeRacerFromField(String racer){
+        nascarDriver currentDriver= polePosition;
+
+        while(currentDriver!=null){
+            if(currentDriver.next.driverName==racer){
+                currentDriver.next=currentDriver.next.next;
+                return;
+            }
+            currentDriver=currentDriver.next;
+        }
+    }
+    public void removeEvenPlacedRacers(){
+        nascarDriver currentDriver= polePosition;
+      
+        while(currentDriver.next!=null){
+            currentDriver.next=currentDriver.next.next;
+            System.out.println(currentDriver.driverName);
+          currentDriver=currentDriver.next;
+        }
+        
+
+        
+      
+    }
+    public void removeDriverWithNumber(int num){
+        nascarDriver currentDriver= polePosition;
+
+        while(currentDriver!=null){
+            if(currentDriver.next.number==num){
+                currentDriver.next=currentDriver.next.next;
+
+                return;
+            }
+            currentDriver=currentDriver.next;
+        }
+    }
+
+}
